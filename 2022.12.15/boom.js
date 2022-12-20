d0c24b4ddffc02ffc420e30a6a93723fd7fee9eb
sasajovkovic@gmail.com <sasajovkovic@gmail.com>
2022-12-20 21:47:09 +0100

    Završeni zadaci sa vežbi održanih 15.12.2022

--- 2022.12.15/boom.js
+++ 2022.12.15/boom.js
@@ -70,24 +70,78 @@ function set() {
 }
 set();
 function readprint() {
-        var item = {};
-        item = window.sessionStorage.getItem("message")
-        if (Object.keys(item).length != 0) {
-            console.log("This is stored data: " + item);
-        }
-        else {
-            console.log("There is no data!");
-        }
+    var item = {};
+    item = window.sessionStorage.getItem("message")
+    if (Object.keys(item).length != 0) {
+        console.log("This is stored data: " + item);
     }
-    readprint();
-    function clearData() {
-        window.sessionStorage.clear();
+    else {
+        console.log("There is no data!");
     }
-    // clearData();
-    function readPrint() {
-            console.log("This is stored data: " + window.sessionStorage.getItem("message"));
+}
+readprint();
+function clearData() {
+    window.sessionStorage.clear();
+}
+// clearData();
+function readPrint() {
+    console.log("This is stored data: " + window.sessionStorage.getItem("message"));
+}
+readPrint();
+function twoPagesBack() {
+    window.history.go(-2);
+}
+// function hello() {
+//     alert("Hello, Sasa!");
+// }
+// hello();
+// function confirmHello() {
+//     confirm("Is everything ok?");
+// }
+// confirmHello();
+// function answerHello() {
+//     prompt("How are you feeling today?");
+// }
+// answerHello();
+function random(min, max) {
+    var arr = new Array();
+    for (var i = 0; i < 10; i++) {
+        arr.push(Math.random() * (max - min)) + min;
     }
-    readPrint();
-    function twoPagesBack() {
-        window.history.go(-2);
-    }
+    return arr;
+}
+// console.log(random(1, 50));
+function randomRound(min, max) {
+    var arr = random(min, max);
+    var newArr = arr.map(function(a) {
+        return a.toFixed(2);
+    })
+    return newArr;
+}
+console.log(randomRound(1, 50));
+function floorRandom(min, max) {
+    var arr = random(min, max);
+    var newArr = arr.map(function(a) {
+        return Math.floor(a);
+    })
+    return newArr;
+}
+console.log(floorRandom(1, 50));
+function max(min, max) {
+    var arr = random(min, max);
+    var max = Math.max(...arr);
+    return max;
+}
+console.log((max(1, 50)));
+function date() {
+    var date = new Date();
+    return date;
+}
+console.log(date());
+function dateTime() {
+    var date = new Date();
+    var dateShort = date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
+    var time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    return dateShort + "\n" + time;
+}
+console.log(dateTime());
